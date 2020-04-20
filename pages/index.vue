551dac12a7a7b6c26e038f622c2a0678041daec2
Emre Tinaz <emre.tinaz@trivago.com>
2020-04-20 10:02:40 +0200

    Initial changes

--- pages/index.vue
+++ pages/index.vue
@@ -1,65 +1,79 @@
 <template>
   <v-layout column justify-center align-center>
-    <v-flex xs12 sm8 md6>
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">Welcome to the Vuetify + Nuxt.js template</v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the
-            <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-            >documentation</a>.
-          </p>
-          <p>
-            If you have questions, please join the official
-            <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              title="chat"
-            >discord</a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github
-            <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              title="contribute"
-            >issue board</a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em>
-              <small>&mdash; John Leider</small>
-            </em>
-          </div>
-          <hr class="my-3" />
-          <a href="https://nuxtjs.org/" target="_blank">Nuxt Documentation</a>
-          <br />
-          <a href="https://github.com/nuxt/nuxt.js" target="_blank">Nuxt GitHub</a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn color="primary" nuxt to="/inspire">Continue</v-btn>
-        </v-card-actions>
-      </v-card>
+    <v-flex xs12 sm8 md8>
+      <p class="text-justify">
+        This platform created to heal the relationship problem
+        between citizens and politicians/political parties. Hopefully, this
+        platform may help citizens and political parties to understand each other
+        better.
+      </p>
+
+      <p class="text-justify">
+        There are 3 goals of this platform :
+      </p>
+
+      <p class="text-justify">
+        Goal I: Understanding each party's purpose. Most of the citizens can
+        only name a few political parties however there are ususaly more than thier aknowlegde registered parties to
+        vote on. Therefore, this platform aims to help citizens to get to know
+        every single option when it comes to voting. Many people do not like
+        politics and do not feel like voting for any party at all because they
+        do not like the popular parties or do not know of the unpopular parties
+        because of the media influence. Therefore, in this platform every party
+        will be portrayed equally and hopefully, citizens will gain access to
+        knowledge of not well-known parties as well.
+      </p>
+
+      <p class="text-justify">
+        Goal II: This platform also aims to find out what people think about
+        each party, positively and negatively. Therefore we can see what parties
+        good and bad at according to the citizens. Then we can easily compare
+        parties, and politicians can also understand what citizens think about
+        them in general.
+      </p>
+
+      <p class="text-justify">
+        Goal III: Eventually, this platform will also help citizens to
+        express their ideas or desires from an imaginary political party, what
+        to focus on, and what to avoid the most. Therefore, political parties
+        can see what citizens care about the most and the least.
+      </p>
     </v-flex>
+    <v-flex xs12 sm8 md8 class="mt-12">
+      <h2 class="text-justify font-weight-bold">Available countries</h2>
+    </v-flex>
+
+    <v-container>
+      <v-row>
+        <v-card class="mx-auto ma-3" max-width="300" to="/inspire">
+          <v-img
+            class="white--text align-end"
+            height="200px"
+            src="https://cdn.pixabay.com/photo/2017/12/23/23/37/flag-of-turkey-3036191_960_720.jpg"
+          >
+          </v-img>
+          <v-card-text class="text--primary text-center">
+            <div>Turkey</div>
+          </v-card-text>
+        </v-card>
+        <v-card class="mx-auto ma-3" max-width="300">
+          <v-img
+            class="white--text align-end"
+            height="200px"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Flag_of_Germany.svg/1280px-Flag_of_Germany.svg.png"
+          >
+          </v-img>
+          <v-card-text class="text--primary text-center">
+            <div>Germany</div>
+          </v-card-text>
+        </v-card>
+      </v-row>
+    </v-container>
   </v-layout>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
 export default {
-  components: {
-    Logo,
-    VuetifyLogo
-  }
+  components: {}
 }
 </script>
