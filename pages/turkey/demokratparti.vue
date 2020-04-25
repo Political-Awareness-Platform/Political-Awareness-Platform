c5dbd9e0c32932b298563f4b52c0569e0b905878
Emre Tinaz <emre.tinaz@trivago.com>
2020-04-25 15:49:21 +0200

    party page required data details added

--- pages/turkey/demokratparti.vue
+++ pages/turkey/demokratparti.vue
@@ -1,6 +1,26 @@
 <template>
   <div>
-    <p>Demokrat Parti</p>
+    <div><p>Demokrat Parti</p></div>
+    <div>
+      <br />
+      Amblemi <br />
+      Adi <br />
+      Kisalmasi <br />
+      Kurulus tarihi<br />
+      Kurucu lider <br />
+      Fiziksek adresi Internet adresi <br />
+      Telefon numarasi <br />
+      Genel baskani <br />
+      Iletisim sozcusu <br />
+      Uye sayisi <br />
+      Sosyal medya linkleri <br />
+      Tuzukteki kurulus amaci <br />
+      Secim vaadleri
+    </div>
+    <hr>
+    <div>
+      pozitif & negatif yorum alani
+    </div>
   </div>
 </template>
 
