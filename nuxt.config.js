7d8d429df3273822955943fad3d3b1d3fbf34026
Emre TINAZ <emre.tinaz@trivago.com>
2020-07-13 20:49:24 +0200

    font added in nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,7 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },{ rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap" }]
   },
   /*
   ** Global CSS
