844070bd5fe8082cb501cc5bc26907029ef0506f
Emre TINAZ <emretinaz@gmail.com>
2020-10-17 21:23:44 +0200

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -56,11 +56,6 @@ export default {
   },
   env: {
     apiKey: process.env.APIKEY,
-    authDomain: process.env.AUTHDOMAIN,
-    databaseURL: process.env.DATABASEURL,
-    projectId: process.env.PROJECTID,
-    storageBucket: process.env.STORAGEBUCKET,
-    messagingSenderId: process.env.MESSAGINGSENDERID,
     appId: process.env.APPID
   },
   server: {
