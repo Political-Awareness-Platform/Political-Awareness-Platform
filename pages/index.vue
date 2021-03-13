04e4470d124eabeb46863a28b7bc36d3ea16df4a
Emre TINAZ <emretinaz@gmail.com>
2021-03-13 16:11:33 +0100

    test2
    test2

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <h1>Development</h1>
+    <h1>Development Continues</h1>
     <p class="platform-purpose"> {{ $t('HomePage.PlatformGoal') }} </p>
     <br />
 
