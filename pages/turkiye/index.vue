c165a867919df4165f208916e854158054cc589f
Emre TINAZ <emretinaz@gmail.com>
2024-01-11 11:25:28 +0100

    Update dependencies and fix styling in turkiye page

--- pages/turkiye/index.vue
+++ pages/turkiye/index.vue
@@ -1,27 +1,32 @@
 <template>
   <div class="p-2 mx-auto">
-    <div class="flex flex-col lg:flex-row lg:flex-col-2 space-y-4 justify-around my-8">
+    <div class="flex flex-col justify-around my-8 space-y-4 lg:flex-row lg:flex-col-2">
       <div>
         <img width="100%" src="~/assets/flags/turkiye.jpg" alt="country-flag" class="rounded">
       </div>
       <div>
-        <p><b>{{ $t('CountryPage.ActivePoliticParties') }} : </b>120</p>
-        <p><b>{{ $t('CountryPage.PartiesinParlament') }} : </b>11</p>
-        <p><b>{{ $t('CountryPage.NumberofVoters') }} : </b>57 058 636</p>
-        <p><b>{{ $t('CountryPage.ContributiontoElection') }} : </b>84.67%</p>
+        <p><b>{{ $t('CountryPage.ActivePoliticParties') }} : </b> <a target="_blank"
+            href="https://www.yargitaycb.gov.tr/icerik/1093/siyasi-parti-genel-bilgileri"
+            class="underline text-sky-800">137</a></p>
+        <p><b>{{ $t('CountryPage.PartiesinParlament') }} : </b>15</p>
+        <p><b>{{ $t('CountryPage.NumberofVoters') }} : </b>64 191 285</p>
+        <p><b>{{ $t('CountryPage.ContributiontoElection') }} : </b> <a target="_blank"
+            href="https://www.ysk.gov.tr/doc/dosyalar/docs/14Mayis2023CBSecimIstatistik.pdf"
+            class="underline text-sky-800">87.05%</a>
+        </p>
       </div>
     </div>
 
-    <div class="flex flex-row flex-wrap justify-center items-stretch gap-6 lg:gap-8 my-12">
+    <div class="flex flex-row flex-wrap items-stretch justify-center gap-6 my-12 lg:gap-8">
       <div v-for="party in parties" :key="party.partyInfo.name">
         <NuxtLink :to="localePath(`/turkiye/${party.partyInfo.dbcode}`)"
-        class="px-3 py-2 border rounded-lg shadow hover:bg-gray-50 lg:px-6 lg:py-3">{{ party.partyInfo.name }}
+          class="px-3 py-2 border rounded-lg shadow hover:bg-gray-50 lg:px-6 lg:py-3">{{ party.partyInfo.name }}
         </NuxtLink>
       </div>
     </div>
     <div class="flex justify-center">
-      <UButton as="NuxtLink" :to="pageLink" target="_blank" color="gray" variant="solid" icon="i-heroicons-pencil-square"
-        >{{ $t('CountryPage.EditThisPage') }}</UButton>
+      <UButton as="NuxtLink" :to="pageLink" target="_blank" color="gray" variant="solid" icon="i-heroicons-pencil-square">
+        {{ $t('CountryPage.EditThisPage') }}</UButton>
     </div>
   </div>
 </template>
